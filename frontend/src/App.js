7f91794f96c81b35e323921a2fff46628b783268
ankittripathi <ankittripathi1456@gmail.com>
2022-02-26 21:25:09 +0530

    search funcnality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import ProductDetails from "./components/product/ProductDetails";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Route path="/" component={Home} exact />
+          <Route path="/search/:keyword" component={Home} />
+          <Route path="/product/:id" component={ProductDetails} exact />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,38 +1,190 @@
-const Home = () => {
+import React, { Fragment, useState, useEffect } from "react";
+import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+import MetaData from "./layout/MetaData";
+import Product from "./product/Product";
+import Loader from "./layout/Loader";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import { getProducts } from "../redux/actions/productActions";
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
+const Home = ({ match }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
+  const [category, setCategory] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const categories = [
+    "Electronics",
+    "Cameras",
+    "Laptops",
+    "Accessories",
+    "Headphones",
+    "Food",
+    "Books",
+    "Clothes/Shoes",
+    "Beauty/Health",
+    "Sports",
+    "Outdoor",
+    "Home",
+  ];
+
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const {
+    loading,
+    products,
+    error,
+    productsCount,
+    resPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
+
+  const keyword = match.params.keyword;
+
+  useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
+
+    dispatch(getProducts(keyword, currentPage, price, category, rating));
+  }, [dispatch, alert, error, keyword, currentPage, price, category, rating]);
+
+  function setCurrentPageNo(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
+  let count = productsCount;
+  if (keyword) {
+    count = filteredProductsCount;
+  }
+
   return (
-    <div className="container container-fluid">
-      <h1 id="products_heading">Latest Products</h1>
-      <section id="products" class="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="https://m.media-amazon.com/images/I/61pBvlYVPxL._AC_UY218_.jpg"
-                alt=""
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                  <a href="#">AmazonBasics High-Speed HDMI Cable, 6 Feet</a>
-                </h5>
-                <div className="ratings mt-auto">
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star"></i>
-                  <i className="fa fa-star-half-o"></i>
-                  <i className="fa fa-star-o"></i>
-                  <span id="no_of_reviews">(5 Reviews)</span>
-                </div>
-                <p className="card-text">$75.56</p>
-                <a href="#" id="view_btn" className="btn btn-block">
-                  View Details
-                </a>
-              </div>
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={"Buy Best Products Online"} />
+
+          <h1 id="products_heading">Latest Products</h1>
+
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {keyword ? (
+                <Fragment>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+
+                      <hr className="my-5" />
+
+                      <div className="mt-5">
+                        <h4 className="mb-3">Categories</h4>
+
+                        <ul className="pl-0">
+                          {categories.map((category) => (
+                            <li
+                              style={{
+                                cursor: "pointer",
+                                listStyleType: "none",
+                              }}
+                              key={category}
+                              onClick={() => setCategory(category)}
+                            >
+                              {category}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+
+                      <hr className="my-3" />
+
+                      <div className="mt-5">
+                        <h4 className="mb-3">Ratings</h4>
+
+                        <ul className="pl-0">
+                          {[5, 4, 3, 2, 1].map((star) => (
+                            <li
+                              style={{
+                                cursor: "pointer",
+                                listStyleType: "none",
+                              }}
+                              key={star}
+                              onClick={() => setRating(star)}
+                            >
+                              <div className="rating-outer">
+                                <div
+                                  className="rating-inner"
+                                  style={{
+                                    width: `${star * 20}%`,
+                                  }}
+                                ></div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products.map((product) => (
+                        <Product key={product._id} product={product} col={4} />
+                      ))}
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
+                products.map((product) => (
+                  <Product key={product._id} product={product} col={3} />
+                ))
+              )}
             </div>
-          </div>
-        </div>
-      </section>
-    </div>
+          </section>
+
+          {resPerPage <= count && (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Prev"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"
+                linkClass="page-link"
+              />
+            </div>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
